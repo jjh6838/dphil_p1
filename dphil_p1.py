c7b9f37ca187681942fad5ea9175f2c50d29493c
jjh6838 <jjh6838@gmail.com>
2024-07-11 08:47:52 -0400

    Professor update_rev

--- dphil_p1.py
+++ dphil_p1.py
@@ -5,10 +5,12 @@ from shapely.geometry import Point
 from other_data.ldc_list import ldc_countries
 import itertools
 
+### 1. Renewable Energy (RE) data: "gdf" is the GeoDataFrame containing the RE data
+
 # Path to the Excel dataset
 dataset_path = 're_data/Global-Integrated-Power-June-2024.xlsx'
 
-# Specify the sheet name to be read from the file
+# Specify the sheet name from the file
 sheet_name = 'Powerfacilities'
 
 # Read the dataset from the specified sheet using pandas
@@ -17,7 +19,9 @@ df = pd.read_excel(dataset_path, sheet_name=sheet_name)
 # Convert the DataFrame to a GeoDataFrame
 gdf = gpd.GeoDataFrame(df, geometry=[Point(xy) for xy in zip(df.Longitude, df.Latitude)])
 
-# Replace the path below with the actual path to the .shp file on your system
+
+### 2. World map data: "world" is the GeoDataFrame containing the world map data
+# Path to the .shp file
 shapefile_path = 'ne_110m_admin_0_countries/ne_110m_admin_0_countries.shp'
 
 # Load the world map
@@ -30,7 +34,7 @@ gdf.crs = "EPSG:4326"
 if gdf.crs != world.crs:
     world = world.to_crs(gdf.crs)
 
-####################################################################################################
+### (Pending) LDC overlapping
 # Filter for LDC countries
 # ldc_world = world[world['ISO_A3'].isin(ldc_countries)]
 
@@ -42,10 +46,9 @@ if gdf.crs != world.crs:
 
 # Perform a spatial join between the solar projects and LDC countries
 # ldc_solar_projects = gpd.sjoin(gdf, ldc_world, how="inner", predicate='intersects')
-####################################################################################################
+###
 
-# Plotting
-# Define colors for each project type
+### (Pending) Define colors for each project type
 # color_map = {
 #     'solar': 'red',
 #     'wind': 'orange',
@@ -56,21 +59,26 @@ if gdf.crs != world.crs:
 #     'coal': 'purple',
 #     'oil/gas': 'pink',
 # }
+###
 
-# Define the facility types to plot
+### 3. Plotting the global distribution of renewable energy facilities
+
+# Select the facility types to plot
 facility_types = ['solar', 'wind', 'hydropower', 'oil/gas']
 
 # Iterate over each facility type and create a map
 for facility_type in facility_types:
-    # Filter for current facility type
-    current_facilities = gdf[gdf['Type'].str.lower() == facility_type]
+    # Filter for the selected facility types
+    selected_facilities = gdf[gdf['Type'].str.lower() == facility_type]
 
     # Filter for "operating" facilities
-    operating_facilities = current_facilities[current_facilities['Status'] == 'operating']
+    operating_facilities = selected_facilities[selected_facilities['Status'] == 'operating']
+    print(f'Number of operating {facility_type}: {len(operating_facilities)}')
 
     # Filter for "planned" facilities (construction, pre-construction, announced)
     planned_statuses = ['construction', 'pre-construction', 'announced']
-    planned_facilities = current_facilities[current_facilities['Status'].isin(planned_statuses)]
+    planned_facilities = selected_facilities[selected_facilities['Status'].isin(planned_statuses)]
+    print(f'Number of planned {facility_type}: {len(planned_facilities)}')
 
     # Plotting
     fig, ax = plt.subplots(figsize=(10, 6))
@@ -79,13 +87,13 @@ for facility_type in facility_types:
     world.plot(ax=ax, color='lightgray')
 
     # Plot "operating" facilities in green
-    operating_facilities.plot(ax=ax, marker='o', color='green', markersize=1, label=f'Operating {facility_type.title()}', alpha=0.1)
+    operating_facilities.plot(ax=ax, marker='o', color='green', markersize=1, label=f'Operating {facility_type.title()}', alpha=0.15)
 
     # Plot "planned" facilities in red
-    planned_facilities.plot(ax=ax, marker='x', color='red', markersize=1, label=f'Planned {facility_type.title()}', alpha=0.1)
+    planned_facilities.plot(ax=ax, marker='o', color='red', markersize=1, label=f'Planned {facility_type.title()}', alpha=0.15)
 
     # Legend and titles
-    ax.legend()
+    ax.legend(markerscale=5)
     ax.set_title(f'Global Distribution of {facility_type.title()} Facilities (Ver 0.1 - Ji)')
 
     # Save the figure to a JPG file
